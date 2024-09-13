98d21cd2fbc20d32d373f44e2ec7bcb0765ba2a7
Zerina <zahmetovic1@etf.unsa.ba>
2024-09-13 13:27:55 +0200

    Added letters A,B and L

---
+++ visual_representation_of_creating_dataset.py
@@ -0,0 +1,35 @@
+import mediapipe as mp
+import os
+import cv2
+import matplotlib.pyplot as plt
+
+mp_hands = mp.solutions.hands
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+
+hands = mp_hands.Hands(static_image_mode=True, min_detection_confidence=0.3)
+
+# extracting landmarks from images taken with collect_imgs
+DATA_DIR = './data'
+
+for dir_ in os.listdir(DATA_DIR):
+    for img_path in os.listdir(os.path.join(DATA_DIR, dir_))[:1]: # we iterate in only one image since we do not want to overload our computer
+        img = cv2.imread(os.path.join(DATA_DIR, dir_, img_path))
+        img_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)  # landmark detection is in RGB
+
+        results = hands.process(img_rgb)
+        if results.multi_hand_landmarks:
+            for hand_landmarks in results.multi_hand_landmarks:
+                mp_drawing.draw_landmarks(
+                    img_rgb,
+                    hand_landmarks,
+                    mp_hands.HAND_CONNECTIONS,
+                    mp_drawing_styles.get_default_hand_landmarks_style(),
+                    mp_drawing_styles.get_default_hand_connections_style()
+                )
+
+        plt.figure()
+        plt.imshow(img_rgb)  # matplot also requires RGB
+
+
+plt.show()
